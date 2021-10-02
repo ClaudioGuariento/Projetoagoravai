69429f7fbbefd237be26213fad56f25d2dce9410
Claudio Cesar Guariento <claudiocguariento@hotmail.com>
2021-10-01 23:14:36 -0300

    projeto

---
+++ src/views/Inicio.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="novo-componente">
+    <v-container>
+        <h2 class="text-a5 text-center mb-3 mt-1">Teste</h2>
+        <v-img class="inicio-imagem mb-4" src="https://img.elo7.com.br/product/zoom/26FE809/painel-adesivo-carro-antigo-decorativo-vermelho-veloz-adesivo-decorativo-parede.jpg"></v-img>
+        <p>Teste</p>
+    </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Inicio'
+}
+</script>
+
+<style scoped>
+    .inicio-imagem {
+        border-radius: 3px;
+    }
+</style>
